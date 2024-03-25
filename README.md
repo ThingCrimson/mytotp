142e53b10e1f0e9e3b852548d37a9b853902c36c
ThingCrimson <thingcrimson0@gmail.com>
2024-03-25 14:25:15 +0200

    Release version of mytotp.sh and README.md

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # mytotp
+
 TOTP 2FA without smartphone
+
+Simple replacement of TOTP Authenticator mobile app for POSIX console (using `oathtool`).
+
+## Prerequisits
+
+POSIX-compliant console, with BASH, GNU utils, GNU Privacy Guard and oathtool available.
+
+## Installation
+
+Just place mytotp.sh somewhere within your $PATH, check is +x attribute present.
+
+Create a direcrory for TOTP keys `mkdir -p ~/.config/mytotp`
+
+If you have no suitable GPG key for encrypting of TOTP keys, create it
+`gpg --yes --batch --passphrase 'Some-words' --quick-generate-key "My TOTP"`
+
+## Usage
+
+When you activate 2FA on some service, get TOTP key string, copy it into buffer, then `gpg -e -r "My TOTP" >~/.config/mytotp/SERVID.gpg`, paste string, press Enter and Crtl+D.
+
+For getting TOTP code for this service do `mytotp.sh SERVID`, unlock your GPG key with above passphrase, then wait for 6-digit code (for your convenience script will wait for next 30 second interval before generating, so you will have a maximum of 30 seconds time for use it).
+
+## Afterword
+
+I wrote this script for my own usage, trying to keep a balance between simplicity and usability. So anyone can try to use it, or modify for it's own needs.
+
--- mytotp.sh
+++ mytotp.sh
@@ -1,38 +1,36 @@
-#!/bin/bash -u
+#!/bin/bash
 #
-# for put OATH key to GPG file crypted for 'My TOTP'
-# gpg -e -r 'My TOTP' > KEY-ID.gpg
+# Put TOTP key for service SERVID to GPG file crypted for 'My TOTP'
+#  gpg -e -r 'My TOTP' > ~/.config/mytotp/SERVID.gpg
 
-KEYDIR="~/.mytotp"
-KEYEXT=".gpg"
-KEY_ID="$1"
-TOTPEX="oathtool -b --totp -"
+KEYDIR=~/.config/mytotp
+KEYEXT=.gpg
+SERVID=$1
 
-if [ -z "${KEY_ID}" ] ; then
-  echo -e "Usage: $0 KEY_ID\nKEY_ID is a service name, abbreviated, w/o ext:"
-  ls -1 ${KEYDIR}/*${KEYEXT} | sed -e 's/\/home.*\//  /; s/\.gpg//'
+if [ -z "${SERVID}" ] ; then
+  echo -e "Usage: $0 SERVID\n\tSERVID is a service ID, abbreviated, w/o ext:"
+  find ${KEYDIR}/*${KEYEXT} | sed -e 's/\/home.*\//  /; s/\.gpg//'
   exit 2
 fi
 
-if [ ! -f "${KEYDIR}/${KEY_ID}${KEYEXT}" ] ; then
-  echo "No key for ${KEYDIR}/${KEY_ID}${KEYEXT}"
+if [ ! -f "${KEYDIR}/${SERVID}${KEYEXT}" ] ; then
+  echo "No key for ${KEYDIR}/${SERVID}${KEYEXT}"
   exit 1
 fi
 
-OKEY=$(gpg -d --quiet "${KEYDIR}/${KEY_ID}${KEYEXT}")
+OKEY=$(gpg -d --quiet "${KEYDIR}/${SERVID}${KEYEXT}")
 
 NOWS=$(date +'%S')
-# depending on seconds: 00-10, 30-40 no wait?
-WAIT=$(( 60 - $NOWS))
+WAIT=$((60 - NOWS))
 if [ ${WAIT} -gt 30 ]; then
-  WAIT=$(( $WAIT - 30 ))
+  WAIT=$((WAIT - 30))
 fi
 echo -n "Seconds :${NOWS} (wait ${WAIT}) ... "
 sleep ${WAIT}
 
-OATH=$(echo "${OKEY}" | ${TOTPEX} )
+OATH=$(echo "${OKEY}" | oathtool -b --totp - )
 
-echo ${OATH}
+echo "${OATH}"
 OKEY="none"
 
 exit 0
